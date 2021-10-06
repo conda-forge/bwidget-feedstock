3393e5066275c9a33f2e1a43ea16108d145d1a21
NandanNayak <50039622+nandandevinagara@users.noreply.github.com>
2021-10-06 20:39:14 +0530

    xcopy command is updated with correct dest. path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 set TCL_LIB_PATH=%PREFIX%\lib\%PKG_NAME%%PKG_VERSION%
 mkdir %TCL_LIB_PATH%
 xcopy src\*.tcl %TCL_LIB_PATH%\
-xcopy src\images %TCL_LIB_PATH%\
-xcopy src\lang %TCL_LIB_PATH%\
+xcopy src\images %TCL_LIB_PATH%\images\
+xcopy src\lang %TCL_LIB_PATH%\lang\
