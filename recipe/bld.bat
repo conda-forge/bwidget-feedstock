a9c3996b34c812256845e83b6d67780735b52c99
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-10-16 09:06:30 +0530

    Source path of license.txt is updated in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,4 +3,4 @@ mkdir %TCL_LIB_PATH% || exit /b 1
 xcopy src\*.tcl %TCL_LIB_PATH%\ || exit /b 1
 xcopy src\images %TCL_LIB_PATH%\images\ || exit /b 1
 xcopy src\lang %TCL_LIB_PATH%\lang\ || exit /b 1
-copy LICENSE.txt %TCL_LIB_PATH%\ || exit /b 1
+copy src\LICENSE.txt %TCL_LIB_PATH%\ || exit /b 1
